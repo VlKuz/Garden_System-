e7e13b6b2356a4fd3cc8a95c3141bb683cce308a
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-22 17:33:17 +0300

    comments were added

--- main.php
+++ main.php
@@ -3,26 +3,26 @@ class Tree
 {
     public $id, $type, $fruits, $weight;
 
-    function to_garden($arr){
+    function to_garden($arr){//Метод класса для добавления объектов в массив "Сад"
         array_push($arr, $this);
         return $arr;
     }
 }
 
-$garden = array();
-$fruits_count = 0;
-$apple_count = 0;
-$apples_weight = 0;
-$pear_count = 0;
-$pears_weight = 0;
+$garden = array(); //Массив "Сад"
+$fruits_count = 0; //Общее количество фруктов
+$apple_count = 0; //Количество яблок
+$apples_weight = 0; //Общий вес яблок
+$pear_count = 0; //Количество груш
+$pears_weight = 0; //Общий вес груш
 
 for($i=0;$i<10;$i++){
-    $apple_tree = new Tree();
-    $apple_tree->id = uniqid();
-    $apple_tree->type = 'Яблоня';
-    $apple_tree->fruits = rand(40,50);
+    $apple_tree = new Tree(); //Создания объекта класса Tree
+    $apple_tree->id = uniqid();//Уникальный id
+    $apple_tree->type = 'Яблоня';//Тип дерева
+    $apple_tree->fruits = rand(40,50);//Количество фруктов на одном дереве
     $num_of_apples=$apple_tree->fruits;
-    for($j = 0;$j<$num_of_apples;$j++)
+    for($j = 0;$j<$num_of_apples;$j++)//Цикл для высчитывания всех фруктов на одном дереве
         $apple_tree->weight+=rand(150,180);
     $garden = $apple_tree->to_garden($garden);
 }
@@ -32,20 +32,21 @@ for($i=0; $i<15; $i++){
     $pear->id = uniqid();
     $pear->type = 'Груша';
     $pear->fruits = rand(0,20);
-    for($j = 0;$j<$num_of_apples;$j++)
+    $num_of_pears=$pear->fruits;
+    for($j = 0;$j<$num_of_pears;$j++)
         $pear->weight+=rand(130,170);
     $garden = $pear->to_garden($garden);
 }
 
 foreach($garden as $row){
-    $fruits_count+=$row->fruits;
+    $fruits_count+=$row->fruits;//Подсчет общего количества фруктов
     if($row->type=='Яблоня'){
-        $apple_count+=$row->fruits;
-        $apples_weight+=$row->weight;
+        $apple_count+=$row->fruits;//Подсчет количества всех яблок
+        $apples_weight+=$row->weight;//Подсчет общего веса яблок
     }
     elseif($row->type=='Груша'){
-        $pear_count+=$row->fruits;
-        $pears_weight+=$row->weight;
+        $pear_count+=$row->fruits;//Подсчет количества всех груш
+        $pears_weight+=$row->weight;//Подсчет общего веса груш
     }
 }
 
