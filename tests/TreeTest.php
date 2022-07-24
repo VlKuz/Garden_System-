6ba953fdb16cf656c59a062cd842701d91fb11c5
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-24 23:30:43 +0300

    test functions were created

--- tests/TreeTest.php
+++ tests/TreeTest.php
@@ -1,11 +1,19 @@
 <?php
 
-class TreeTest extends \PHPUnit\Framework\TestCase{
-    function testTo_garden() {
+class TreeTest extends \PHPUnit\Framework\TestCase {
 
+    function testTo_garden() {
         $tree = new App\Tree;
+        $arr= array();
         $result = $tree->to_garden($arr);
-        $this->assertEquals($arr, $result);
+        $this->assertContains($tree, $result);
+    }
+
+    function testWeight_counting() {
+        $tree = new App\Tree;
+        $tree->fruits = 100;
+        $tree->weight_counting(150,180);
+        $this->assertNotEmpty($tree->weight);
     }
 }
 ?>
